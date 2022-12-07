60f33810e47ab40d0aae4bb313ebb98bc8e231ce
masud90895 <masudhossainmbs129@gmail.com>
2022-12-07 20:19:28 +0600

    react csv file to json converter added

--- src/Component/From/FromTwo.js
+++ src/Component/From/FromTwo.js
@@ -1,8 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FromContext } from '../../Context/FromProvider';
+import { useCSVReader } from 'react-papaparse';
 
 const FromTwo = () => {
+    const [csvData,setCsvData]=useState([])
+    console.log('csvData',csvData.data);
+
+    const {CSVReader} = useCSVReader()
 
     const {fromOneData} = useContext(FromContext)
 
@@ -11,8 +16,39 @@ const FromTwo = () => {
         clientName,
         contractorName} = fromOneData;
 
-    console.log(fromOneData);
     
+        const styles = {
+            csvReader: {
+                display: 'flex',
+                flexDirection: 'row',
+                marginBottom: 10,
+            },
+            browseFile: {
+                width: '40%',
+            },
+            acceptedFile: {
+                border: '1px solid #ccc',
+                height: 45,
+                lineHeight: 2.5,
+                paddingLeft: 10,
+                width: '200px',
+                margin: "5px"
+            },
+            remove: {
+                width: '40%',
+                borderRadius: "5px",
+                padding: '0 20px',
+
+            },
+            progressBarBackgroundColor: {
+                backgroundColor: 'blue',
+            },
+        };
+    
+
+
+
+
     return (
         <>
          <h2 className="text-xl font-semibold leading-7 text-gray-800 mt-10">
@@ -40,21 +76,46 @@ const FromTwo = () => {
                     </div>
                 </div>
 
-                <div className=" z-10 top-0 w-full h-full flex mt-6 bg-opacity-60">
+
+                <CSVReader
+                onUploadAccepted={(results) => {
+                    setCsvData(results)
+                }}
+            >
+                {({
+                    getRootProps,
+                    acceptedFile,
+                    ProgressBar,
+                    getRemoveFileProps
+                }) => (
+                    <>
+                        <div style={styles.csvReader} className=" z-10 top-0 w-full h-full flex mt-6 bg-opacity-60">
     <div className="extraOutline p-4 bg-white w-max m-auto rounded-lg">
         <div className="file_upload p-5 relative border-4 border-dotted border-gray-300 rounded-lg md:w-[450px]" >
             <svg className="text-indigo-500 w-24 mx-auto mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" /></svg>
             <div className="input_field flex flex-col w-max mx-auto text-center">
                 <label>
                     <input className="text-sm cursor-pointer w-36 hidden" type="file" multiple />
-                    <div className="text bg-indigo-600 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-indigo-500">Select</div>
+                    <div>
+                    <button type='button'  {...getRootProps()} style={styles.browseFile} className="text bg-indigo-600 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-indigo-500">Select</button>
+                    <button  {...getRemoveFileProps()} style={styles.remove}  className=" btn btn-sm mx-auto bg-red-600 hover:bg-red-400 text-white border-none ml-4">Remove</button> 
+                    </div>
                 </label>
-
-                <div className="title text-indigo-500 uppercase">or drop files here</div>
+                <div style={styles.acceptedFile}>
+                                {acceptedFile && acceptedFile.name}
+               </div>                                          
             </div>
         </div>
     </div>
 </div>
+                        <ProgressBar style={styles.progressBarBackgroundColor} />
+                    </>
+                )}
+            </CSVReader>
+
+
+
+
 
                 <div className="mt-12 md:flex items-center">
                     <div className="flex flex-col">
